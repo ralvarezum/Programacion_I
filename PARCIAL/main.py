29e7ce02b8007165df889deac2e4d47e25a67516
Rodrigo Alvarez <rorototi@hotmail.com>
2019-05-16 11:32:54 -0300

    Cambio y fuera

--- PARCIAL/main.py
+++ PARCIAL/main.py
@@ -4,3 +4,16 @@ doc1 = Documento()
 doc1.crear_documento()
 doc1.agregar_pagina()
 doc1.show()
+
+#Guardado de objetos
+import pickle
+
+filehandler = open("Documento.obj","wb")
+pickle.dump(doc1,filehandler)
+filehandler.close()
+
+file = open("Documento.obj",'rb')
+object_file = pickle.load(file)
+file.close()
+
+print(object_file.show)
